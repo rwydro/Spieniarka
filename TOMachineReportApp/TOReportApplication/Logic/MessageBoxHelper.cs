62494f0299f5ecbc7de895bf2e5d29513be17fc6
rwydro <radwyd@gmail.com>
2019-01-06 23:37:37 +0100

    Dodanie Timera part 2

---
+++ TOMachineReportApp/TOReportApplication/Logic/MessageBoxHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TOReportApplication.Logic
+{
+    public static class MessageBoxHelper
+    {
+        public static void ShowMessageBox(string info, MessageBoxIcon icon)
+        {
+            MessageBox.Show(info, "Error", MessageBoxButtons.OK,
+                icon);
+        }
+    }
+}
---
+++ TOMachineReportApp/TOReportApplication/Logic/MyTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Timers;
+
+namespace TOReportApplication.Logic
+{
+    public class MyTimer
+    {
+        private Timer timer;
+        public event EventHandler Elapsed;
+
+        public void SetTimer()
+        {
+            timer = new Timer(90*000);
+            timer.Elapsed += new ElapsedEventHandler(OnTimerElapsed);
+        }
+
+        public void ResetTimer()
+        {
+            if (timer.Enabled)
+            {
+                timer.Stop();
+            }
+
+            timer.Start();  
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            if (Elapsed != null)
+                Elapsed(sender, e);
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+}
